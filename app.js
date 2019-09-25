417426b8cbedc60dc1099dad7ffa1f71071dbf38
Norman Lin <normanlin.net@gmail.com>
2019-09-25 13:32:19 +0800

    add cheatsheet: carry, release and emt(1)

---
+++ app.js
@@ -0,0 +1,26 @@
+const display = (title, content) => {
+  document.getElementById("myLinks").style.display = "none";
+  document.getElementById("content").innerHTML = content;
+  document.getElementById("title").innerHTML = title;
+};
+
+function toggleMenu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+document
+  .getElementById("carry")
+  .addEventListener("click", () => display("CARRY", contentCarry));
+
+document
+  .getElementById("release")
+  .addEventListener("click", () => display("RELEASE", contentRelease));
+
+document
+  .getElementById("emt")
+  .addEventListener("click", () => display("EMT", contentEMT));
---
+++ content.js
@@ -0,0 +1,429 @@
+const contentEMT = `
+<div class="card">
+<div class="card-header">放下溺者 (單人拖帶)</div>
+<details>
+  <summary class="card-body">向後跨一大步 > 轉身放下溺者 > 清除口腔異物 > 將溺者平放 > CPR姿勢預備</summary>
+  <div class="card-detail">
+    <p>
+        預備動作: 雙手鎖溺者肩部、身體緊貼溺者背部。
+    </p>
+    <p>
+      1. 向後跨一大步: 統一跨左腳。
+    </p>
+    <p>
+      2. 轉身放下溺者: 轉側身,小心放下溺者,同時順勢屈膝成高跪姿,穿腋下手手肘著地,利用大腿和手夾住溺者使其側躺,平行面向
+      河(海)岸。
+    </p>
+    <p>
+      3. 清除口腔異物: 先暢通溺者呼吸道,穿腋下手以姆、中指成七形指,用食指由上而下,由內而外,由上而下,由內而外,清除口中
+      異物2次,溺者頭向前微傾,使異物可順利流出。
+    </p>
+    <p>
+      4. 將溺者平放: 確認異物清除完畢,護頸扶腰,翻轉溺者。上方手扶頸、下方手扶腰同時扣住內側手,用大腿帶引小心放下溺者。
+    </p>
+    <p>
+      5. CPR姿勢預備: 雙腳跪起,上方腳對齊溺者肩線,大腿貼緊溺者手臂。
+    </p>
+  </div>
+</details>
+</div>
+
+<div class="card">
+<div class="card-header">復甦姿勢 (適用無外傷病患)</div>
+<details>
+  <summary class="card-body">內側手與肩同高 > 外側腳往內交叉 > 外側手彎曲置於胸 > 向內翻轉溺者 > 救者將褲腰手換至後頸部 > 溺者上方手置於臉頰,上方腳彎向腹部 > 持續觀察</summary>
+  <div class="card-detail">
+    <p>
+        預備動作: 溺者平躺 / 救者跪在溺者胸旁約一個拳頭距離。
+    </p>
+    <p>
+      1. 內側手與肩同高: 把溺者手臂平移與肩同高,手呈自然姿勢,掌心向上。
+    </p>
+    <p>
+      2. 外側腳往內交叉: 將溺者外側腳抬起與內側腳交叉。
+    </p>
+    <p>
+      3. 外側手彎曲置於胸: 將溺者外側手上彎置於胸部。
+    </p>
+    <p>
+      4. 向內翻轉溺者: 救者一手置於頸部穩定,另一手抓住對側褲腰處,將傷患
+      身體轉向施救者。(膝蓋頂住溺者身體緩緩後退)
+    </p>
+    <p>
+      5. 救者將褲腰手換至後頸部: 溺者側臥姿勢穩定時,救者把置於褲腰處的手,小心地置換至後頸部,注意不可抬起傷患的頭部。
+    </p>
+    <p>
+        6. 溺者上方手置於臉頰,上方腳彎向腹部: 把溺者上方的手置於臉頰下方,手掌貼地,救者小心抽出傷患頭頸下方的手,再將上方的腳彎向腹部。
+    </p>
+    <p>
+      7. 持續觀察: 持續觀察溺者呼吸及循環狀況直到恢復意識或有專業人員接手。
+    </p>
+  </div>
+</details>
+</div>
+
+<div class="card">
+<div class="card-header">溺者嘔吐</div>
+<details>
+  <summary class="card-body">將溺者外側手貼齊耳際 > 護頸扶腰 > 向外翻轉溺者 > 暢通呼吸道 > 清除口中異物 > 平放溺者 > 重新評估,檢查脈搏</summary>
+  <div class="card-detail">
+    <p>
+      1. 將溺者外側手貼齊耳際: 將溺者外側手上舉貼齊耳際。
+    </p>
+    <p>
+      2. 護頸扶腰: 救者上方手保護溺者頸部,下方手扶腰。
+    </p>
+    <p>
+      3. 向外翻轉溺者: 護頸扶腰、大腿緊靠溺者背部,小心翻轉溺者使之側躺。
+    </p>
+    <p>
+      4. 暢通呼吸道: 下方手穿過溺者腋下並且手肘著地,採高跪姿利用身體及手夾住溺者,小心調整頭部使其呼吸道暢通。
+    </p>
+    <p>
+      5. 清除口中異物: 以姆、中指成七形指,用食指由上而下,由內而外,由上而
+      下,由內而外,清除口中異物兩次,溺者頭向前微傾使異物可順利流出
+    </p>
+    <p>
+        6. 平放溺者: 用大腿帶引溺者,護頸扶腰,慢慢將溺者平放。
+    </p>
+    <p>
+      7. 重新評估,檢查脈搏: 重新進行CPR流程。
+    </p>
+  </div>
+</details>
+</div>
+
+<div class="card">
+    <div class="card-header">
+      <a href="https://drive.google.com/open?id=1NkOKfPmO3sRZSBT7JQXD6fezTSj7Kz23">急救法(一)講義</a>
+    </div>
+  </div>
+`;
+
+const contentRelease = `
+  <div class="card">
+  <div class="card-header">正面抱頭解脫</div>
+  <details>
+    <summary class="card-body">吸收撥 > 扣腰 > 轉升 > 托顎頂背</summary>
+    <div class="card-detail">
+      <p>
+        1. 吸收撥: 吸氣、收下顎、撥水下沉
+      </p>
+      <p>
+        2. 扣腰: 雙手掌心向內、虎口向上扣住溺者腰部。
+      </p>
+      <p>
+        3. 轉升: 將溺者轉 180 度使其背對救者,
+        同時踩水上升將溺者上推出水
+        面,使其臉出水面。
+      </p>
+      <p>
+        4. 托顎、頂背: 內側手(控制力弱)先托顎,外側手
+        (抱腰手)再頂背。
+      </p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">正面纏頸解脫</div>
+  <details>
+    <summary class="card-body">吸收撥 > 托顎夾鼻 > 壓肩轉升 > 放夾鼻手 > 托顎頂背</summary>
+    <div class="card-detail">
+      <p>
+        1. 吸收撥: 吸氣、收下顎、撥水下沉
+      </p>
+      <p>
+          2.托顎夾鼻: 伸手自救者與溺者之間穿過,掌
+          心掌根封口拖顎,食指中指夾鼻
+          使其無法呼吸亦不會嗆水。
+      </p>
+      <p>
+          3.壓肩轉升: 另一手穿過溺者腋下,反手向內
+          按壓溺者肩膀,救者低頭自溺者
+          腋下穿過,將溺者旋轉 180 度,
+          同時踩水出水面。
+      </p>
+      <p>
+        4. 放夾鼻手: 出水面後放開溺者口鼻,使其呼
+        吸(掌根繼續托顎控制溺者)。
+      </p>
+      <p>
+          5. 托顎、頂背: 內側手(控制力弱)先托顎,外側手
+          (抱腰手)再頂背。
+        </p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">背面纏頸解脫</div>
+  <details>
+    <summary class="card-body">吸收撥 > 手掌手肘 > 扭轉關節 > 平移後方 > 托顎頂背</summary>
+    <div class="card-detail">
+      <p>
+        1. 吸收撥: 吸氣、收下顎、撥水下沉
+      </p>
+      <p>
+          2.手掌手肘: 判斷溺者的下方手,救者接近溺者下
+          方手手掌側的手(手掌手),去抓溺者
+          手掌 (由外向內、掌心向內四指向下);
+          救者接近溺者手肘側的手(手肘手)去
+          抓手肘關節(由外向內、掌心向內四
+          指朝上)。
+      </p>
+      <p>
+          3.扭轉關節: 手掌手向下、向外扭轉溺者手腕關
+          節;手 肘手用力按手肘麻筋,逼使溺
+          者鬆手。 (右手吃漢堡、左手摩托車)
+      </p>
+      <p>
+        4. 平移後方: 拖高溺者肘關節,並自溺者腋下退至溺者背後。
+      </p>
+      <p>
+          5. 托顎、頂背: 踩水出水面,放手肘手,手肘手確實
+          托顎,手掌手才能去頂背。
+        </p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">抓腕解脫</div>
+  <details>
+    <summary class="card-body">吸收撥 > 抓對側手 > 下壓扭開 > 腳壓手拉 > 托顎頂背</summary>
+    <div class="card-detail">
+      <p>
+        1. 吸收撥: 吸氣、收下顎、撥水下沉
+      </p>
+      <p>
+          2.抓對側手: 救者未被抓住的手(自由手)虎口朝前
+          掌心朝下抓溺者對側手腕。
+          (若救者自由手為左手,則溺者對側
+          手為左手、溺者同側手為右手)
+      </p>
+      <p>
+          3.下壓扭開: 救者自由手手臂與手肘向下壓溺者同
+          側手 (壓在手肘與手腕中點),同時被
+          纏住的手(受制手)向上、向內扭開。
+      </p>
+      <p>
+        4. 腳壓手拉: 救者手握緊溺者對側手,用同側腳之
+        腳掌壓溺者肩窩,使溺者轉180 度背
+        對救者(此時手同時向外翻轉並往耳
+        際後方拉)。
+        救者踩水上升,使溺者口鼻出水面。
+      </p>
+      <p>
+          5. 托顎、頂背: 自由手拖顎,另一隻手再頂背。
+        </p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">
+    <a href="https://drive.google.com/open?id=1lIc6C70wQBujyngrI_5caGMC_mYSA_HN">解脫法講義</a>
+  </div>
+  </div>
+`;
+
+const contentCarry = `
+  <div class="card">
+    <div class="card-header">摟胸帶⼈法</div>
+    <details>
+      <summary class="card-body">托顎頂背 > 摟胸 > (鎖扣) > 拖帶</summary>
+      <div class="card-detail">
+        <p>
+          1. 托顎、頂背
+        </p>
+        <p>
+          2. 摟胸: 頂背手由上而下越過溺者
+          胸前,扣緊腋下,肘關節平貼溺
+          者胸前,髖骨頂住溺者使其浮出
+          水面。
+        </p>
+        <p>
+          * 鎖扣: 溺者掙扎,將托顎手穿
+          過溺者腋下與摟胸手相扣。
+        </p>
+        <p>
+          3. 拖帶: 側泳前進。
+        </p>
+      </div>
+    </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">托顎鎖腕帶人法</div>
+  <details>
+    <summary class="card-body">托顎頂背 > 換手托顎 > 鎖腕 > 拖帶</summary>
+    <div class="card-detail">
+      <p>
+        1. 托顎、頂背
+      </p>
+      <p>
+        2. 換手托顎: 原頂背手從溺者腋下
+        穿過用掌心托住溺者下顎。 </p>
+      <p>
+        3. 鎖腕: 原托顎手從另一腋下穿
+        過,抓住溺者同側手腕上拉置於
+        胸前。
+      </p>
+      <p>
+        4. 拖帶: 基本仰泳前進。
+      </p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">雙手鎖肩帶人法</div>
+  <details>
+    <summary class="card-body">托顎頂背 > 鎖肩 > 拖帶</summary>
+    <div class="card-detail">
+      <p>
+        1. 托顎、頂背
+      </p>
+      <p>
+        2. 鎖肩: 頂背手先動,托顎手隨
+        後,雙手穿過溺者腋下回扣,用
+        大臂將溺者肩膀鎖住。 </p>
+      <p>
+        3. 拖帶: 基本仰泳前進。
+      </p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">雙手托臂帶人法</div>
+  <details>
+    <summary class="card-body">托顎頂背 > 托臂 > 拖帶</summary>
+    <div class="card-detail">
+      <p>
+        1. 托顎、頂背
+      </p>
+      <p>
+        2. 托臂: 頂背手先動,托顎手隨
+        後,沿肩膀向下由側邊抓住溺者
+        上臂(虎口朝向溺者指尖),掌心
+        向上翻轉夾緊,托臂手伸直。 </p>
+      <p>
+        3. 拖帶: 基本仰泳前進。
+      </p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">托顎帶人法</div>
+  <details>
+    <summary class="card-body">托顎頂背 > (換手托顎) > 拖帶</summary>
+    <div class="card-detail">
+      <p>
+        1. 托顎、頂背: 使溺者身體浮出水
+        面、口鼻露出水面。
+      </p>
+      <p>
+        2. (換手托顎)。 </p>
+      <p>
+        3. 拖帶: 托顎手臂伸直,側泳前進。
+      </p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">抓腕帶人法</div>
+  <details>
+    <summary class="card-body">托顎頂背 > 抓腕 > 拖帶</summary>
+    <div class="card-detail">
+      <p>
+        1. 托顎、頂背
+      </p>
+      <p>
+        2. 抓腕: 頂背手掌心朝下、虎口朝
+        前摸著溺者身體,由溺者肩順勢
+        向下抓住同側手腕處,向前往頭
+        上方舉,虎口朝向溺者。 </p>
+      <p>
+        3. 拖帶: 側泳前進,隨時注意溺者
+        的口鼻露出水面。
+      </p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">抓髮帶人法</div>
+  <details>
+    <summary class="card-body">托顎頂背 > 抓髮 > 拖帶</summary>
+    <div class="card-detail">
+      <p>
+        1. 托顎、頂背
+      </p>
+      <p>
+        2. 抓髮: 頂背手自溺者後腦滑向前
+        額抓緊髮根 </p>
+      <p>
+        3. 拖帶: 側泳前進。
+      </p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">抓衣帶人法</div>
+  <details>
+    <summary class="card-body">托顎頂背 > 抓衣 > 拖帶</summary>
+    <div class="card-detail">
+      <p>
+        1. 托顎、頂背
+      </p>
+      <p>
+        2. 抓衣: 頂背手抓住溺者衣領或背
+        後衣服。(旋緊)</p>
+      <p>
+        3. 拖帶: 側泳或基本仰泳前進。
+      </p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">乏泳帶人法 - 扶肩</div>
+  <details>
+    <summary class="card-body">扶肩(面對面) > 分腿 > 拖帶</summary>
+    <div class="card-detail">
+      <p>
+        扶肩(面對面): 救者游至溺者前
+        一公尺處緊急停游,安慰溺者
+        後,請溺者雙手扶在肩上、頭向
+        後仰。溺者雙腿分開輕靠在救者
+        腰側。抬頭蛙泳前進。
+      </p>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">乏泳帶人法 - 搭肩</div>
+  <details>
+    <summary class="card-body">搭肩(一前一後) > 拖帶</summary>
+    <div class="card-detail">
+      <p>
+        搭肩(一前一後): 救者游至溺者
+        前一公尺處緊急停游,安慰溺者
+        後,救者轉身,請溺者雙手搭在
+        救者肩膀上。抬頭蛙泳前進,溺
+        者可助游。</p>
+    </div>
+  </details>
+  </div>
+
+  <div class="card">
+  <div class="card-header">
+    <a href="https://drive.google.com/open?id=1Z-ODHNN1iZ7GYs_fwF8tBJdu-GV2KmGw">帶人法講義</a>
+  </div>
+</div>
+`;
